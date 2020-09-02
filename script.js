7b4eb3d042b2963fd7c67c314023f8f03a4b73e7
Paul B <paul.belfrage@gmail.com>
2020-09-02 11:38:00 +0200

    init
    init

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLabels = document.querySelectorAll('.lbl-toggle');
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+/* https://www.digitalocean.com/community/tutorials/css-collapsible */
